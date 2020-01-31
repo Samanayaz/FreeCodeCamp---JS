10ba9d58cc5f5add9f65cfa92378f8a6955c573f
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-01-31 00:05:15 -0600

    update

---
+++ recursion.js
@@ -0,0 +1,26 @@
+// Replace Loops using Recursion
+
+// sum function
+
+function sum(arr, n) {
+    // Only change code below this line
+  if(n<=0){
+    return arr[0];}
+    else {
+      return sum(arr, n-1) + arr[n];
+    }
+  }
+    // Only change code above this line
+  
+  
+  console.log(sum([2,3,4],1));
+
+//   next multiply
+
+  function multiply(arr, n) {
+    if (n <= 0) {
+      return arr[0];
+    } else {
+      return multiply(arr, n - 1) * arr[n];
+    }
+  }
