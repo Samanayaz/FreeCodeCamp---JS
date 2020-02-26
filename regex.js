345f14eea0787bfe0f745d678b11179047656804
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-02-25 23:58:46 -0600

    Regex

--- regex.js
+++ regex.js
@@ -15,4 +15,12 @@ let result = twinkleStar.match(starRegex); // Change this line
 console.log(result);
 
 
-// test method the regex comes first and string goes inside the parenthesis while in the match method regex is inside the parenthesis of method. 
+// => test method the regex comes first and string goes inside the parenthesis while in the match method regex is inside the parenthesis of method. 
+
+
+// You can search for multiple patterns using the alternation or OR operator: |. 
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+console.log(result);
