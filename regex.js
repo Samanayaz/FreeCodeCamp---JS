5cac857af9db03c8d9fbbf85aef176c3eaa1673a
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-02-25 23:55:28 -0600

    Redex

---
+++ regex.js
@@ -0,0 +1,18 @@
+// test method 
+// The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), and returns true or false if your pattern finds something or not.
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
+
+
+
+// Match method
+//  to also extract the actual matches you found with the .match() method.
+// using g flag for extract repitative pattern, and i flag to avoid case sensitivity 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+console.log(result);
+
+
+// test method the regex comes first and string goes inside the parenthesis while in the match method regex is inside the parenthesis of method. 
