f511ec8eb0c089e19f6ee84807c9a0d1f0be1c67
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-05 23:39:39 -0600

    myMap

--- prototype.js
+++ prototype.js
@@ -16,3 +16,27 @@ let funModule = (function() {
   })();
 
 //   Note that you have an immediately invoked function expression (IIFE) that returns an object motionModule. This returned object contains all of the mixin behaviors as properties of the object. The advantage of the module pattern is that all of the motion behaviors can be packaged into a single object that can then be used by other parts of your code.
+
+
+// useful array function is Array.prototype.filter(), or simply filter()./////////////
+
+// filter calls a function on each element of an array and returns a new array containing only the elements for which that function returns true. In other words, it filters the array, based on the function passed to it. Like map, it does this without needing to modify the original array.
+
+// The global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+
+  // Add your code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  // Add your code above this line
+
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
