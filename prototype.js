40febc8bae103dd911eadb47703fd1e979bb5f27
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-06 23:56:56 -0600

    update

--- prototype.js
+++ prototype.js
@@ -40,3 +40,21 @@ Array.prototype.myMap = function(callback) {
 var new_s = s.myMap(function(item) {
   return item * 2;
 });
+
+
+// We have defined a function named squareList. You need to complete the code for the squareList function using any combination of map(), filter(), and reduce() so that it returns a new array containing only the square of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it. An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2].
+
+// 
+
+const squareList = (arr) => {
+  // Only change code below this line
+  const squareList = arr =>
+  arr
+    .filter(num => num > 0 && num % parseInt(num) === 0)
+    .map(num => Math.pow(num, 2));
+  return arr;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
