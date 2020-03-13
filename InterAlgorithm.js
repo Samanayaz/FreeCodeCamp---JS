60c05d3b3143f60cf5b454b133665aa11052d2ae
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-12 22:42:08 -0500

    update new algorithm

--- InterAlgorithm.js
+++ InterAlgorithm.js
@@ -20,3 +20,46 @@ function sumPrimes(num) {
     }
     return x !== 1 && x !== 0;
   }
+
+
+  //////////////////////////next 
+//   Fill in the object constructor with the following methods below:
+
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// // setFullName(firstAndLast)
+
+
+  var Person = function(firstAndLast) {
+    var fullName = firstAndLast;
+  
+    this.getFirstName = function() {
+      return fullName.split(" ")[0];
+    };
+  
+    this.getLastName = function() {
+      return fullName.split(" ")[1];
+    };
+  
+    this.getFullName = function() {
+      return fullName;
+    };
+  
+    this.setFirstName = function(name) {
+      fullName = name + " " + fullName.split(" ")[1];
+    };
+  
+    this.setLastName = function(name) {
+      fullName = fullName.split(" ")[0] + " " + name;
+    };
+  
+    this.setFullName = function(name) {
+      fullName = name;
+    };
+  };
+  
+  var bob = new Person("Bob Ross");
+  bob.getFullName();
