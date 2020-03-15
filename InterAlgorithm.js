6e0f47cfd60c7bf9fb6cf7471b7bab0cd5976e46
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-14 23:36:21 -0500

    numberCheck

--- InterAlgorithm.js
+++ InterAlgorithm.js
@@ -63,3 +63,13 @@ function sumPrimes(num) {
   
   var bob = new Person("Bob Ross");
   bob.getFullName();
+
+  // Return true if the passed string looks like a valid US phone number.
+
+// The user may fill out the form field any way they choose as long as it has the format of a valid US numbe\
+
+function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+telephoneCheck("555-555-5555");
