0b77a1b9d3f7a225014a24b0ea3495409d13771b
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-17 23:15:40 -0500

    palindrome

---
+++ finalAlg.js
@@ -0,0 +1,48 @@
+// Convert the given number into a roman numeral.
+
+var convertToRoman = function(num) {
+  var deciVal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+
+  var roman = "";
+
+  for (var index = 0; index < deciVal.length; index++) {
+    while (deciVal[index] <= num) {
+      roman += romanNumeral[index];
+      num -= deciVal[index];
+    }
+  }
+
+  return roman;
+};
+
+// next 
+// PalindromeChheck
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+function palindrome(str) {
+  return (
+    str.replace(/[\W_]/g, "").toLowerCase() ===
+    str
+      .replace(/[\W_]/g, "")
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
