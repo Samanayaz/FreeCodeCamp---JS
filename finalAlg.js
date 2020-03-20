013102b26e9d15a763f5765e2d0ae0fbe14ce3d0
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2020-03-19 23:53:38 -0500

    numberCheck

--- finalAlg.js
+++ finalAlg.js
@@ -49,3 +49,8 @@ function palindrome(str) {
 
 // 
 // Return true if the passed string looks like a valid US phone number.
+function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+telephoneCheck("555-555-5555");
